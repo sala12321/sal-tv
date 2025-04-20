14f62072d0d7bd8051f0a06150aee6fb5856114b
Salahadin Admasu <104188060+sala12321@users.noreply.github.com>
2025-04-20 15:23:44 +0300

    &hearts;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-6 text-gray-400 text-center">
-          <p>&copy; 2025 SazTv. Made with &love; by <a><b>Salahadin</b></a></p>
+          <p>&copy; 2025 SazTv. Made with &hearts; by <a><b>Salahadin</b></a></p>
         </div>
       </div>
     </footer>
